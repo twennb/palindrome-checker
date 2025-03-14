7275e19f48d342c11581ad8fcee78160a0ed93ea
twennb <tom.wennberg@hotmail.com>
2025-03-14 13:23:50 +0100

    Initial commit with program plan and structure

---
+++ palindrome.py
@@ -0,0 +1,27 @@
+"""A program that checks whether a word or sentence is a palindrome"""
+# plan:
+# get user input to check as palindrome
+# see if input == backwards input
+# if not, not a palindrome
+# if yes, palindrome
+# ask to check another input or exit
+
+
+def user_input():
+    """gets input from user that they wish to check for palindrome"""
+
+
+def reverse_input():
+    """reverses the user's input"""
+
+
+def check_palindrome():
+    """checks whether a passed input is the same backwards as forwards"""
+
+
+def main():
+    """the main function"""
+
+
+if __name__ == "__main__":
+    main()
