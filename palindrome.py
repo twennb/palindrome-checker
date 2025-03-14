e1783f7bbca915e4ec467e06f8169be46769f3b6
twennb <tom.wennberg@hotmail.com>
2025-03-14 13:56:31 +0100

    added a check_again function, asking if the user wants to continue with more prompts or stop

--- palindrome.py
+++ palindrome.py
@@ -5,6 +5,7 @@
 # if not, not a palindrome
 # if yes, palindrome
 # ask to check another input or exit
+import sys
 
 
 def get_user_input():
@@ -12,7 +13,8 @@ def get_user_input():
     print("=================================")
     while True:
         validation_list = []
-        palindrome_input = input("Please enter your palindrome query: ")
+        palindrome_input = input(
+            "Enter a word or message that you want to find out whether it is a palindrome or not\n-: ")
         for character in palindrome_input:
             if character.isalpha() or character.isspace():
                 pass
@@ -32,10 +34,24 @@ def reverse_input(palindrome_input):
 
 def check_palindrome(normal, reverse):
     """checks whether a passed input is the same backwards as forwards"""
-    if normal == reverse:
+    if normal.lower() == reverse.lower():
         print(f"\nYour input '{normal}' is a palindrome!")
     else:
         print(f"\nYour input '{normal}' is not a palindrome!")
+
+
+def check_again():
+    """asks the user if they want to check another input"""
+    while True:
+        user_choice = input(
+            "Would you like to check a different input? (yes/no)\n-: ")
+        if user_choice == "yes":
+            main()
+        elif user_choice == "no":
+            print("\nGoodbye!\n")
+            sys.exit()
+        else:
+            print("Invalid input!")
 
 
 def main():
@@ -43,6 +59,7 @@ def main():
     potential_palindrome = get_user_input()
     reverse_palindrome = reverse_input(potential_palindrome)
     check_palindrome(potential_palindrome, reverse_palindrome)
+    check_again()
 
 
 if __name__ == "__main__":
